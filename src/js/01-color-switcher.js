0b71dc59e175ffbff354739213c2d78258e9d285
igrok1803444 <v618591@gmail.com>
2023-08-19 23:05:52 +0300

    end task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,30 @@
+//DOM navigation
+const onButton = document.querySelector('button[data-start]');
+const offButton = document.querySelector('button[data-stop]');
+//variables
+let timerId = null;
+
+//event listeners 
+onButton.addEventListener('click', () => {
+    if (timerId !== null) {
+          onButton.setAttribute('disabled' , '' );
+    }
+    onButton.setAttribute('disabled', '');
+    offButton.removeAttribute('disabled');
+    timerId = setInterval(changeBodyColor, 1000);
+})
+offButton.addEventListener('click', () => {
+    if (timerId !== null) {
+        onButton.removeAttribute('disabled'); 
+        offButton.setAttribute('disabled' , '' );
+        clearInterval(timerId);
+        timerId = null;
+    }
+})
+//functions
+function changeBodyColor() {
+    document.body.style.backgroundColor = `${getRandomHexColor()}`
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
