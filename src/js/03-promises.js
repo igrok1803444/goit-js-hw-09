76602345f3b38d0311d07265d37001e7adfaf5a0
igrok1803444 <v618591@gmail.com>
2023-08-20 15:25:16 +0300

    end task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+//imports
+import { Notify } from 'notiflix';
+//DOM navigation
+const form = document.querySelector('.form');
+//variables
+let firstDelay = 0;
+let step = 0;
+let amount = 0;
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  firstDelay = Number(form.delay.value);
+  step = Number(form.step.value);
+  amount = Number(form.amount.value);
+  for (let i = 0; i < amount; i++) {
+    createPromise(i, firstDelay);
+
+    firstDelay += step;
   }
+});
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve(Notify.success(`Fulfilled promise ${position} in ${delay}ms`));
+      } else {
+        // Reject
+        reject(Notify.failure(`Rejected promise ${position} in ${delay}ms`));
+      }
+    }, delay);
+  });
 }
